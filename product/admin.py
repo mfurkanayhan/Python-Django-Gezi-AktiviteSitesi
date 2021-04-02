95baccf1e417f1056d62d8e5be7f75adba735234
mfurkanayhan <mfurkana067@gmail.com>
2021-04-02 23:19:07 +0300

    4 Django E ticaret Model Category ve Product ilişkisi

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Category
+from product.models import Category, Content
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'city', 'country', 'konum', 'status']
+    list_filter = ['status','category']
+
+
 admin.site.register(Category,CategoryAdmin)
+admin.site.register(Content,ContentAdmin)
--- product/models.py
+++ product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     STATUS = (
@@ -9,12 +10,34 @@ class Category(models.Model):
     title = models.CharField(max_length=30)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    image = models.ImageField(blank=True,upload_to='images/')
+    image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
-    slug = models.SlugField()
-    parent = models.ForeignKey('self',blank=True, null=True ,related_name='children', on_delete=models.CASCADE)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Content(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # relation with Category table
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    detail = models.TextField()
+    city = models.TextField()
+    country = models.TextField()
+    konum = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
