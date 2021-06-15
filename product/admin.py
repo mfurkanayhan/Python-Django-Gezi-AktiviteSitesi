2e8b0a1b2e80d4c44515195031f1ad8d1266ff53
mfurkanayhan <mfurkana067@gmail.com>
2021-06-16 01:03:07 +0300

    22 Django E Ticaret Kategori ve Urun için otomatik slag oluşturma

--- product/admin.py
+++ product/admin.py
@@ -10,17 +10,20 @@ class ContentImageInline(admin.TabularInline):
     model = Images
     extra = 5
 
-
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status', 'image_tag']
     list_filter = ['status']
     readonly_fields = ('image_tag',)
 
+
+
+
 class ContentAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'city', 'country', 'konum', 'image_tag', 'status']
-    readonly_fields = ('image_tag',)
+    readonly_fields = ('image_tag','catimg_tag',)
     list_filter = ['status', 'category']
     inlines = [ContentImageInline]
+    prepopulated_fields = {'slug': ('title',)}
 
 
 
@@ -33,6 +36,7 @@ class CategoryAdmin2(DraggableMPTTAdmin):
     list_display = ('tree_actions', 'indented_title',
                     'related_contents_count', 'related_contents_cumulative_count')
     list_display_links = ('indented_title',)
+    prepopulated_fields = {'slug': ('title',)}
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
--- product/models.py
+++ product/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 from django.forms import ModelForm
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
 from mptt.fields import TreeForeignKey
@@ -19,7 +20,7 @@ class Category(MPTTModel):
     description = models.CharField(blank=True, max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(null=False, unique=True)
     parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -40,6 +41,10 @@ class Category(MPTTModel):
             return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
 
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+
 class Content(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -51,7 +56,7 @@ class Content(models.Model):
     description = models.CharField(blank=True, max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
     detail = RichTextUploadingField()
-    slug = models.SlugField(blank=True, max_length=150)
+    slug = models.SlugField(null=False, unique=True)
     city = models.TextField()
     country = models.TextField()
     konum = models.TextField()
@@ -67,6 +72,12 @@ class Content(models.Model):
         if self.image:
             return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
+
+    def catimg_tag(self):
+        return mark_safe((Category.status))
+
+    def get_absolute_url(self):
+        return reverse('content_detail', kwargs={'slug': self.slug})
 
 class Images(models.Model):
     content=models.ForeignKey(Content,on_delete=models.CASCADE)
