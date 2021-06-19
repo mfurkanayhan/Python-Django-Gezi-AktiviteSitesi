21d17dc3d17b87cc091f48c8fc4ab4fd199a1429
mfurkanayhan <mfurkana067@gmail.com>
2021-06-20 00:58:28 +0300

    26,5 Django E Ticaret Uye ekleme sırasında otomatik profil oluşturma

--- home/views.py
+++ home/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from home.forms import SearchForm, SignUpForm
-from home.models import Setting, ContactFormu, ContactFormMessage
+from home.models import Setting, ContactFormu, ContactFormMessage, UserProfile
 from product.models import Content, Category, Images, Comment
 
 
@@ -161,6 +161,13 @@ def signup_view(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
+            #Create data in profile table for user
+            current_user = request.user
+            data=UserProfile()
+            data.user_id=current_user.id
+            data.image="images/users/user.png"
+            data.save()
+            messages.success(request, " Hoşgeldiniz.. ")
             return HttpResponseRedirect('/')
 
     form = SignUpForm()
